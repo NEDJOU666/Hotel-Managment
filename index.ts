3d7574fa031c6b39ad41a6d7773c55336546cef7
06Veve <mbunaverlaine@gmail.com>
2024-09-16 10:01:11 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+// pages/index.js
+import HelpButton from '../components/HelpButton';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Support Page</h1>
+      <HelpButton />
+    </div>
+  );
+}
