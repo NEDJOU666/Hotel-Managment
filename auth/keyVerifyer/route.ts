44525f6c379c107d379e232d98bf7425f696f3be
Choclark <147842539+Choclark@users.noreply.github.com>
2024-09-17 12:41:29 +0100

    Delete auth directory

--- auth/keyVerifyer/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server'
-export async function POST(request:Request) {
-    const key  = await request.json()
-    console.log(key)
-    return NextResponse.json({data:key})
-}
